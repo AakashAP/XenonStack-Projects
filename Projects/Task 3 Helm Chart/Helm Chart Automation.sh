7af77b4399632f80b591ad223b1aed9b70e294e9
Aakash Maurya <aakashmaurya745@gmail.com>
2025-03-14 21:03:59 +0530

    Update

---
+++ Projects/Task 3 Helm Chart/Helm Chart Automation.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+REPO="https://github.com/AakashAP/XenonStack-Projects.git"
+    DIR="XenonStack-Projects/Projects/Task 1 Docker"
+
+
+
+
+echo "PACKAGE REQUIREMENTS"
+echo "---------------------------------------------------"
